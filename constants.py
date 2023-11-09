e78219e4295e9cb3a4eab5a9e2ecef4f8e51d335
Shamika Tissera <nimeshshamika@gmail.com>
2023-11-09 16:24:54 +0530

    removed env constants

--- constants.py
+++
@@ -1,9 +0,0 @@
-##### JWT Constants #####
-JWT_SECRET_KEY = "hftAs9zyQCf5"
-
-##### MySQL DB Constants #####
-MYSQL_HOST = "localhost"
-MYSQL_USER = "root"
-MYSQL_PASSWORD = ""
-MYSQL_DB = "distributed_banking_app"
-MYSQL_PORT = "3306"

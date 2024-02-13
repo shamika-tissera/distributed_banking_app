093c1a2fbc34d8204dcfea3013a5c12e1ec49cb4
Chirath Fonseka <chirathfonseka98#gmail.com>
2024-02-14 00:58:10 +0530

    create spring boot project

---
+++ services/account_management/src/test/java/mit/project/account_management/AccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package mit.project.account_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
